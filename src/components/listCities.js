0073b11d62a4af9ce51356e911273affd1abb0f4
Sqizm <ntantashev@gmail.com>
2024-08-30 21:02:52 +0300

    version 0.1.0

---
+++ src/components/listCities.js
@@ -0,0 +1,22 @@
+// Для отображения названия города.
+export const cityNames = {
+    Moscow: 'Москве',
+    'Saint Petersburg': 'Санкт-Петербурге',
+    Volgograd: 'Волгограде',
+    Menzelinsk: 'Мензелинске',
+    Kazan: 'Казани',
+    Samara: 'Самаре',
+    Krasnodar: 'Краснодаре',
+    Agryz: 'Агрызе'
+};
+
+// Для формы выбора города.
+export const cities = [
+    { value: "Moscow", label: "Москва"},
+    { value: "Saint Petersburg", label: "Санкт-Петербург"},
+    { value: "Volgograd", label: "Волгоград"},
+    { value: "Menzelinsk", label: "Мензелинск"},
+    { value: "Kazan", label: "Казань"},
+    { value: "Samara", label: "Самара"},
+    { value: "Krasnodar", label: "Краснодар"},
+];
